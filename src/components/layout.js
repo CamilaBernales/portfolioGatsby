eef07186923ec7cf64dd2a2b57e6508dfe8f39df
Camila Bernales <camilabernales09@gmail.com>
2020-07-08 22:16:43 -0300

    dark theme not ready yet

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,27 +5,40 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
 import "./layout.css"
+import "./index.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
+  const [checked, setChecked] = useState(
+    localStorage.getItem("theme") === "dark" ? true : false
+  )
+  useEffect(() => {
+    document
+      .getElementsByTagName("body")[0]
+      .setAttribute("data-theme", localStorage.getItem("theme"))
+  }, [checked])
+  const toggleThemeChange = () => {
+    if (checked === false) {
+      localStorage.setItem("theme", "dark")
+      setChecked(true)
+    } else {
+      localStorage.setItem("theme", "light")
+      setChecked(false)
     }
-  `)
-
+  }
   return (
     <>
-      <Header/>
+      <Header />
+      <input
+        type="checkbox"
+        defaultChecked={checked}
+        onChange={() => toggleThemeChange()}
+      />
+
       <div
         style={{
           margin: `0 auto`,
